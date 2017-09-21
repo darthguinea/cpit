4980534264358f0e8e8cfd2138682e0f441ae9d4
darthguinea <darthguinea@protonmail.com>
2017-09-21 17:48:09 +1000

    Setup script added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='cpit',
+    version='0.1',
+    author='darthguinea',
+    author_email='darthguinea@protonmail.com',
+    platforms='any',
+    install_requires=['pyperclip']
+    )
+
